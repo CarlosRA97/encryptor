6825306275b46a3271c0a66eb5ad10275b11c499
Carlos Rivero Aro <Carlos@MacBook-Pro-de-Carlos.local>
2016-04-10 14:38:52 +0200

    init commit

---
+++ encrypt.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	HashIt(readFromUser())
+}
+
+// HashIt This funcion hash the string given
+// and send it directly to the clipboard
+func HashIt(text string) {
+	data := []byte(text)
+	hash := fmt.Sprintf("%x\n", sha1.Sum(data))
+	fmt.Println(hash)
+	clipboardIt(hash)
+}
+
+func readFromUser() string {
+	var myname string
+	str1 := "What you want to hash?"
+	fmt.Println(str1)
+	fmt.Scanf("%s", &myname)
+	// fmt.Println("Hello", myname)
+	return myname
+}
+
+func clipboardIt(data string) {
+	// commands := [4]string{"echo ", data, " | ", "pbcopy"}
+	// fmt.Println(co)
+	command := fmt.Sprintf("echo %s | pbcopy", data)
+	fmt.Println(command)
+	exec.Command(command).Start()
+}
